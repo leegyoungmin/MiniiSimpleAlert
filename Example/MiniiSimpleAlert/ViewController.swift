3cccd4a8f265823791c58738dbb06d6fcad196a6
미니 <cow970814@naver.com>
2022-10-30 17:42:06 +0900

    feat : add Simple Alert

--- Example/MiniiSimpleAlert/ViewController.swift
+++ Example/MiniiSimpleAlert/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MiniiSimpleAlert
 
 class ViewController: UIViewController {
 
@@ -14,11 +15,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showAlert(_ sender: UIButton) {
+        
+        MiniiSimpleAlert(title: "Show Alert", confirm: "Ok") {
+            print("It is Simple Alert")
+        }.show(in: view)
+        
     }
-
+    
 }
 
